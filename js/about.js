8125b7c28377266ddff4f1adae6bed3f73c66a30
adonProjects <support.adon@zohomail.in>
2021-09-06 10:42:42 +0530

    update about.html added about us section

--- js/about.js
+++ js/about.js
@@ -1,19 +1,34 @@
+'use strict';
+
 // getting DOM Elements 
 const navigation = document.querySelector('.nav--area');
 const header = document.querySelector('.header_area');
 
+const sections = document.querySelectorAll('.section');
+const lines = document.querySelectorAll('.line');
+
 window.addEventListener('load' , function(){
     document.querySelector('.loader-wrapper').classList.add('load--fade--out');
-
+    
+    
     // variables--------------------------------
     const navigationHeight = navigation.getBoundingClientRect().height;
-
-
+    
+    
     // function for sticky navigation bar 
     const stickyNav = function(entries){
         const [entry] = entries;
         if (!entry.isIntersecting) navigation.classList.add('sticky')
         else navigation.classList.remove('sticky')
+    }
+    
+    // reveal section function 
+    const revealSection = function(entries , observer){
+        const [entry] = entries;
+        console.log(entries);
+        if (!entry.isIntersecting) return;
+        entry.target.classList.remove('section--hidden');
+        observer.unobserve(entry.target);
     }
     
     // for sticky navigation bar 
@@ -25,6 +40,27 @@ window.addEventListener('load' , function(){
             rootMargin : `-${navigationHeight}px`
         }
     );
-    headerObserevr.observe(header);    
-
-})
+    
+    
+    
+    // for section fade in effect 
+    const sectionObserver = new IntersectionObserver(
+        revealSection , 
+        {
+            root : null,
+            threshold : .15
+        }
+    )
+    
+    sections.forEach(function(section){
+        sectionObserver.observe(section);
+        section.classList.add('section--hidden');
+    })
+    
+    headerObserevr.observe(header);
+    
+    // scroll to top 
+    const button = document.querySelector("#back2top");
+    BACK2TOP(button, 1000);
+    
+});
